06cd8622f10d79a65293ff54feeeee2ee77a6b67
sgwood <sgwood@qq.com>
2023-04-03 11:02:47 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/webIndex', 'IndexController@index');
+Route::get('users', function()
+{
+    return 'Users!';
+});
+Route::get('/user/{id}', [UserController::class, 'show']);
