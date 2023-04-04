521dc08aac2e93c93c162604d505b85b20a46cf9
sgwood <sgwood@qq.com>
2023-04-04 17:39:29 +0800

    index fixed.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,18 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\View\View;
-
 
 
 class IndexController extends Controller
 {
 
     protected array $webInfo = [
-        'companyId' => '100070',
-        'domain' => '',
-        'domainScheme' => '',
 
         'cacheBug' => '0',
         'cacheTTL' => '300',
@@ -21,22 +16,11 @@ class IndexController extends Controller
         'cacheReplaceA' => '1',
         'mtimeTTL' => '2592000',
         'throttle' => '30',
-        'hasMobile' => '1',
-
-        'optimizeDomain' => '180.76.55.80',
-        'sourceDomain' => 'xiaohucloud.com',
-        'imgDomain' => 'img-xhyftp.xiaohucloud.cn',
-        'env' => ' ',
+        'sourceDomain' => 'cadz.org.cn',
         'cacheDir' => 'cache',
         'cacheSuffix' => 'html',
         'uri' => '',
-        'diyDomain' => 'diy-xhyftp.xiaohucloud.cn',
-        'cosDomain' => 'cos-xhyftp.xiaohucloud.cn',
-        'isMobile' => false,
-        'sftp' => false,
-        'debug' => true,
         'optimizeUrl' => '',
-        'sourceUrl' => '',
         'cacheFlag' => '',
         'savePath' => '',
         'path' => '',
@@ -47,21 +31,21 @@ class IndexController extends Controller
     public function index(): View
     {
         try {
-            $this->webInfo['uri']=$_SERVER['REQUEST_URI'];
+            $this->webInfo['uri'] = $_SERVER['REQUEST_URI'];
             $this->main($this->webInfo);
         } catch (Exception $e) {
             header('HTTP/1.1 404 Not Found');
         } catch (Error $e) {
             header('HTTP/1.1 404 Not Found');
         }
-        return view('user.profile');
+        return view('404');
     }
 
 
     function main(&$webInfo)
     {
         $this->init($webInfo);
-        $htmlContent ='';
+        $htmlContent = '';
         if (file_exists($webInfo['savePath'])) {
             $htmlContent = file_get_contents($webInfo['savePath']);
         }
@@ -75,46 +59,27 @@ class IndexController extends Controller
         if ((isset($htmlContent) && $webInfo['isSpider'] == false && $webInfo['throttle'] > 0 && $mtime >= $webInfo['cacheTTL']) || !isset($htmlContent) || ($webInfo['isSpider'] == false && $mtime >= $webInfo['mtimeTTL'])) {
             $throttle = __DIR__ . DIRECTORY_SEPARATOR . '.throttle';
             @file_put_contents($throttle, $webInfo['throttle'] - 1);
-            switch (trim($webInfo['env'])) {
-                case 'local':
-                    break;
-                case 'source':
-                    $trimHtmlContent = trim($htmlContent);
-                    if (isset($htmlContent) && !empty($trimHtmlContent)) {
-                        list($content, $code) = $this->getContent($webInfo['sourceUrl'], $timeout);
-                    } else {
-                        list($content, $code) = $this->getContent($webInfo['sourceUrl'], 15);
-                    }
-                    $trimContent = trim($content);
-                    if ($code == 200 && !empty($trimContent)) {
-                        $htmlContent = $content;
-                    }
-                    break;
-                case 'optimize':
-                default:
-                    $trimHtmlContent = trim($htmlContent);
-                    if (isset($htmlContent) && !empty($trimHtmlContent)) {
-                        list($content, $code) = $this->getContent($webInfo['optimizeUrl'], $timeout);
-                    } else {
-                        list($content, $code) = $this->getContent($webInfo['optimizeUrl'], 15);
-                    }
 
-                    $trimContent = trim($content);
-                    if ($code == 200 && !empty($trimContent)) {
-                        if (mb_strpos($content, '你访问的页面不存在或被删除') === false && mb_strpos($content, '请检查网站的站点设置是否填写完整') === false) {
-                            $htmlContent = $content;
-                        }
-                    }
-                    break;
+            $trimHtmlContent = trim($htmlContent);
+            if (isset($htmlContent) && !empty($trimHtmlContent)) {
+                list($content, $code) = $this->getContent($webInfo['optimizeUrl'], $timeout);
+            } else {
+                list($content, $code) = $this->getContent($webInfo['optimizeUrl'], 15);
             }
+
+            $trimContent = trim($content);
+            if ($code == 200 && !empty($trimContent)) {
+                if (mb_strpos($content, '你访问的页面不存在或被删除') === false && mb_strpos($content, '请检查网站的站点设置是否填写完整') === false) {
+                    $htmlContent = $content;
+                }
+            }
+
 
             $trimHtmlContent = trim($htmlContent);
 
             if (!isset($htmlContent) || empty($trimHtmlContent)) {
                 header('HTTP/1.1 404 Not Found');
-                if (file_exists('404.html')) {
-                    include('404.html');
-                }
+
                 exit;
             } else {
                 $dir = dirname($webInfo['savePath']);
@@ -143,11 +108,7 @@ class IndexController extends Controller
             $doc->substituteEntities = false;
 
             try {
-                if (version_compare(phpversion(), '5.6', '<')) {
-                    $doc->loadHTML(mb_convert_encoding($htmlContent, 'HTML-ENTITIES', 'UTF-8'));
-                } else {
-                    $doc->loadHTML($htmlContent);
-                }
+                $doc->loadHTML($htmlContent);
             } catch (Exception $e) {
             }
             $webInfo['cacheReplaceA'] && $this->cacheReplaceA($doc, $webInfo);
@@ -159,28 +120,6 @@ class IndexController extends Controller
         }
     }
 
-
-    function cacheReplaceA(&$doc, &$webInfo)
-    {
-        $nodes = $doc->getElementsByTagName('a');
-        $type = 'href';
-        $sourceUrlWithoutUri = $this->getSourceUrl($webInfo, false);
-        foreach ($nodes as $key => $item) {
-            foreach ($item->attributes as $k => $v) {
-                if ($k == $type && ($url = $v->value)) {
-                    $replace = '';
-                    if ($webInfo['domainScheme'] && $webInfo['domain']) {
-                        $replace = $webInfo['domainScheme'] . '://' . $webInfo['domain'];
-                    }
-                    $url = trim(str_replace($sourceUrlWithoutUri . '//', $replace . '/', $url));
-                    $url = trim(str_replace($sourceUrlWithoutUri . '?', $replace . '/?', $url));
-                    $url = trim(str_replace($sourceUrlWithoutUri, $replace, $url));
-                    $url = empty($url) ? '/' : $url;
-                    $item->setAttribute($type, $url);
-                }
-            }
-        }
-    }
 
     function getCacheFlag(&$webInfo)
     {
@@ -205,30 +144,6 @@ class IndexController extends Controller
         return sprintf('/%s/%s%s.%s', $webInfo['cacheDir'], $webInfo['path'], $uuid, $webInfo['cacheSuffix']);
     }
 
-    function getSourceUrl(&$webInfo, $optimize = false, $uri = '')
-    {
-        if ($uri) {
-            $replaceUriSuffix = array('index.php');
-            foreach ($replaceUriSuffix as $item) {
-                if (mb_strpos($webInfo['uri'], $item) !== 0) {
-                    continue;
-                }
-                $uri = str_replace($item, '', $webInfo['uri']);
-            }
-        }
-
-        $prefix = 'www';
-        if ($webInfo['hasMobile'] && $webInfo['isMobile']) {
-            $prefix = 'm';
-        }
-
-        if ($optimize) {
-            return sprintf('http://%s/%s.%s.%s%s', $webInfo['optimizeDomain'], $prefix, $webInfo['companyId'], $webInfo['sourceDomain'], $uri);
-        }
-
-        return sprintf('http://%s.%s.%s%s', $prefix, $webInfo['companyId'], $webInfo['sourceDomain'], $uri);
-    }
-
     function getContent($url, $timeout = 60)
     {
         $ch = curl_init();
@@ -245,23 +160,9 @@ class IndexController extends Controller
     function init(&$webInfo)
     {
 
-        $webInfo['isMobile'] =false;
-        $suffix = '_www';
-        $webInfo['isMobile'] && $suffix = '_m';
-        $onlyMobileWeb = explode('.', $webInfo['domain']);
-        if (isset($onlyMobileWeb[0]) && $onlyMobileWeb[0] == 'm') {
-            $webInfo['isMobile'] = true;
-            $suffix = '_m';
-        }
-        $onlyMobileWeb = explode('.', $_SERVER['HTTP_HOST']);
-        if (isset($onlyMobileWeb[0]) && $onlyMobileWeb[0] == 'm') {
-            $webInfo['isMobile'] = true;
-            $suffix = '_m';
-        }
-        $webInfo['cacheDir'] .= $suffix;
+        $webInfo['cacheDir'] .= '_www';
 
-        $webInfo['sourceUrl'] = $this->getSourceUrl($webInfo, false, $webInfo['uri']);
-        $webInfo['optimizeUrl'] = $this->getSourceUrl($webInfo, true, $webInfo['uri']);
+        $webInfo['optimizeUrl'] = sprintf('http://%s.%s%s', 'www', $webInfo['sourceDomain'], $$webInfo['uri']);
 
         $webInfo['cacheFlag'] = $this->getCacheFlag($webInfo);
         $webInfo['savePath'] = __DIR__ . DIRECTORY_SEPARATOR . $webInfo['cacheFlag'];
@@ -285,7 +186,6 @@ class IndexController extends Controller
             $webInfo['isSpider'] = true;
         }
 
-        $webInfo['debug'] && @file_put_contents('debug.json', json_encode($webInfo));
     }
 
 
